df564b7e4c6411304031d7b0560e545f05716658
Aakansha-modi <56475108+Aakansha-modi@users.noreply.github.com>
2021-06-06 21:34:03 +0530

    Update urls.py

--- SUNO/login/urls.py
+++ SUNO/login/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('register', views.register),
     path('here', views.success),
     path('verify/<auth_token>' , views.verify),
+    path('blog', views.add_blog),
+    path('blog_delete/<str:id>',views.blog_delete),
+    path('blog_get', views.blogHome)
 ]
